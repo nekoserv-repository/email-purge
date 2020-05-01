517564d68a1b9ba085a21a1327503e0243a4f944
nekoserv-repository <nekoserv@fai.tld>
2020-05-01 11:22:21 +0200

    Changed : run for ever

--- email_purge.py
+++ email_purge.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 #
-# This small tool is designed to connect to an e-mail account using IMAP.
+# This app is designed to connect to an e-mail account using IMAP.
 # Then it iterates over all emails in a specified folder,
-# report them to a spam service and finally delete them.
+# send them to spamcop.net and finally delete them.
 #
 
-import sys
 import imaplib
-import email
-import datetime
+from time import sleep
 from mailing import mailing
 from config.config import cfg
 from config.config import check_runtime_cfg
@@ -18,25 +16,32 @@ from spamcop import spamcop_http
 ## check runtime configuration
 check_runtime_cfg()
 
-## connect to IMAP with SSL
-M = imaplib.IMAP4_SSL(cfg['EMAIL_SVC_IMAP'])
+while True:
+    ## connect to IMAP with SSL
+    M = imaplib.IMAP4_SSL(cfg['EMAIL_SVC_IMAP'])
 
-try:
-    rv, data = M.login(cfg['EMAIL_ACCOUNT'], cfg['EMAIL_PASSWD'])
-except imaplib.IMAP4.error:
-    print("LOGIN FAILED!!! ", data)
-    exit(1)
+    try:
+        rv, data = M.login(cfg['EMAIL_ACCOUNT'], cfg['EMAIL_PASSWD'])
+    except imaplib.IMAP4.error:
+        print("LOGIN FAILED!!! ", data)
+        exit(1)
 
-## change folder
-rv, data = M.select(cfg['EMAIL_FOLDER'])
-if rv == 'OK':
-    print("> Opening mailbox...")
-    ## do some stuff
-    mailing.process_mailbox(M)
-    print("> Done.")
-    M.close()
-else:
-    print("ERROR: Unable to open mailbox ", rv)
+    ## change folder
+    rv, data = M.select(cfg['EMAIL_FOLDER'])
+    if rv == 'OK':
+        print("> Opening mailbox...")
+        ## do some stuff
+        mailing.process_mailbox(M)
+        print("> Done.")
+        M.close()
+    else:
+        print("ERROR: Unable to open mailbox ", rv)
 
-## exit
-M.logout()
+    ## exit mailbox
+    M.logout()
+    
+    ## info
+    print("~ waiting 10 mins")
+    
+    ## sleep for 10 mins
+    sleep(600)
